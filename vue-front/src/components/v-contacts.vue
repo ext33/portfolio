81d0668e58e3c41292d51875c2235523a481dc70
Egor <redwoodx3@gmail.com>
2020-08-13 14:47:17 +0300

    upd contacts

--- vue-front/src/components/v-contacts.vue
+++ vue-front/src/components/v-contacts.vue
@@ -1,17 +1,138 @@
 <template>
   <div id="Contacts">
-
+    <h1>
+      Contacts
+    </h1>
+    <div class="cont">
+      <div class="logo1">
+        <vs-button shadow icon @click="active=!active" class="logoBtn">
+          <img src="@/assets/contacts/email.svg" class="contactsLogo">
+        </vs-button>
+        <vs-dialog not-close v-model="active">
+          <template>
+            <div class="modalContent">
+              <div class="modalLogo">
+                <img src="@/assets/contacts/email.svg" class="contactsLogo">
+              </div>
+              <div class="modalDesc">
+                <p class="modalText">
+                  You may contact me with email: <br><b><a class="modalLink" href="mailto:redwoodx3@gmail.com" target="_blank">redwoodx3@gmail.com</a></b>
+                </p>
+              </div>
+            </div>
+          </template>
+        </vs-dialog>
+      </div>
+      <div class="logo1">
+        <vs-button shadow icon @click="active2=!active2" class="logoBtn">
+          <img src="@/assets/contacts/telegram.svg" class="contactsLogo">
+        </vs-button>
+        <vs-dialog not-close v-model="active2">
+          <template>
+            <div class="modalContent">
+              <div class="modalLogo">
+                <img src="@/assets/contacts/telegram.svg" class="contactsLogo">
+              </div>
+              <div class="modalDesc">
+                <p class="modalText">
+                  Also you may contact me with telegram: <br><b><a class="modalLink" href="https://t.me/xdxd3xd" target="_blank">@xdxd3xd</a></b>
+                </p>
+              </div>
+            </div>
+          </template>
+        </vs-dialog>
+      </div>
+      <div class="logo1">
+        <vs-button shadow icon @click="active3=!active3" class="logoBtn">
+          <img src="@/assets/contacts/instagram.svg" class="contactsLogo">
+        </vs-button>
+        <vs-dialog not-close v-model="active3">
+          <template>
+            <div class="modalContent">
+              <div class="modalLogo">
+                <img src="@/assets/contacts/instagram.svg" class="contactsLogo">
+              </div>
+              <div class="modalDesc">
+                <p class="modalText">
+                  If you interested, my instagram: <br><b><a class="modalLink" href="https://www.instagram.com/ext33_/" target="_blank">ext33</a></b>
+                </p>
+              </div>
+            </div>
+          </template>
+        </vs-dialog>
+      </div>
+      <div class="logo1">
+        <vs-button shadow icon @click="active4=!active4" class="logoBtn">
+          <img src="@/assets/github-logo.svg" class="contactsLogo">
+        </vs-button>
+        <vs-dialog not-close v-model="active4">
+          <template>
+            <div class="modalContent">
+              <div class="modalLogo">
+                <img src="@/assets/github-logo.svg" class="contactsLogo">
+              </div>
+              <div class="modalDesc">
+                <p class="modalText">
+                  All my projects on github: <br><b><a class="modalLink" href="https://github.com/ext33" target="_blank">ext33</a></b>
+                </p>
+              </div>
+            </div>
+          </template>
+        </vs-dialog>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-name: "Contacts"
+name: "Contacts",
+  data:() => ({
+    active: false,
+    active2: false,
+    active3: false,
+    active4: false,
+  })
 }
 </script>
 
 <style scoped>
 #Contacts{
-  padding-top: 40px;
+  margin: 6% 0;
+  padding-top: 5%;
 }
+.cont{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 20px;
+}
+.contactsLogo{
+  width: 70px;
+  height: 70px;
+}
+.logoBtn{
+  margin: 20px;
+}
+.modalContent{
+  padding: 30px;
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+}
+.modalLogo{
+  float: left;
+}
+.modalDesc{
+  float: right;
+  margin-left: 20px;
+}
+.modalText{
+  font-size: 17px;
+}
+.modalLink{
+  text-decoration: none;
+  color: #2c3e50;
+}
+
 </style>
--- vue-front/src/components/v-stack.vue
+++ vue-front/src/components/v-stack.vue
@@ -12,31 +12,31 @@
               <h2>Frontend</h2>
               <div class="fill">
 
-                <div class="dialog">
+                <div class="logo1">
                   <vs-button shadow icon @click="active=!active" class="dialogButton">
                     <img src="@/assets/js-logo.svg" class="dialogLogo">
                   </vs-button>
                 </div>
 
-                <div class="dialog">
+                <div class="logo1">
                   <vs-button shadow icon @click="active=!active" class="dialogButton">
                     <img src="@/assets/html5-logo.svg" class="dialogLogo">
                   </vs-button>
                 </div>
 
-                <div class="dialog">
+                <div class="logo1">
                   <vs-button shadow icon @click="active=!active" class="dialogButton">
                     <img src="@/assets/css3-logo.svg" class="dialogLogo">
                   </vs-button>
                 </div>
 
-                <div class="dialog">
+                <div class="logo1">
                   <vs-button shadow icon @click="active=!active" class="dialogButton">
                     <img src="@/assets/react-logo.svg" class="dialogLogo">
                   </vs-button>
                 </div>
 
-                <div class="dialog">
+                <div class="logo1">
                   <vs-button shadow icon @click="active=!active" class="dialogButton">
                     <img src="@/assets/vue-logo.svg" class="dialogLogo">
                   </vs-button>
@@ -48,31 +48,31 @@
               <h2>Backend</h2>
               <div class="fill">
 
-                <div class="dialog">
+                <div class="logo1">
                   <vs-button shadow icon @click="active=!active" class="dialogButton">
                     <img src="@/assets/python-logo.svg" class="dialogLogo">
                   </vs-button>
                 </div>
 
-                <div class="dialog">
+                <div class="logo1">
                   <vs-button shadow icon @click="active=!active" class="dialogButton">
                     <img src="@/assets/docker-logo.svg" class="dialogLogo">
                   </vs-button>
                 </div>
 
-                <div class="dialog">
+                <div class="logo1">
                   <vs-button shadow icon @click="active=!active" class="dialogButton">
                     <img src="@/assets/nginx-logo.png" class="dialogLogo">
                   </vs-button>
                 </div>
 
-                <div class="dialog">
+                <div class="logo1">
                   <vs-button shadow icon @click="active=!active" class="dialogButton">
                     <img src="@/assets/postgresql-logo.svg" class="dialogLogo">
                   </vs-button>
                 </div>
 
-                <div class="dialog">
+                <div class="logo1">
                   <vs-button shadow icon @click="active=!active" class="dialogButton">
                     <img src="@/assets/sqlite-logo.svg" class="dialogLogo">
                   </vs-button>
@@ -84,25 +84,25 @@
               <h2>Tools</h2>
               <div class="fill">
 
-                <div class="dialog">
+                <div class="logo1">
                   <vs-button shadow icon @click="active=!active" class="dialogButton">
                     <img src="@/assets/pycharm-logo.svg" class="dialogLogo">
                   </vs-button>
                 </div>
 
-                <div class="dialog">
+                <div class="logo1">
                   <vs-button shadow icon @click="active=!active" class="dialogButton">
                     <img src="@/assets/webstorm-logo.svg" class="dialogLogo">
                   </vs-button>
                 </div>
 
-                <div class="dialog">
+                <div class="logo1">
                   <vs-button shadow icon @click="active=!active" class="dialogButton">
                     <img src="@/assets/vs-logo.png" class="dialogLogo">
                   </vs-button>
                 </div>
 
-                <div class="dialog">
+                <div class="logo1">
                   <vs-button shadow icon @click="active=!active" class="dialogButton">
                     <img src="@/assets/vim-logo.svg" class="dialogLogo">
                   </vs-button>
@@ -134,8 +134,8 @@ export default {
 
 <style scoped>
 #stack{
-  margin: 5% 0;
-  padding-top: 12%;
+  margin: 6% 0;
+  padding-top: 8%;
   /*background-image: -webkit-linear-gradient(275deg, #fff 19%, #DCDCDC	19%);*/
 }
 #cards{
@@ -146,8 +146,9 @@ export default {
   align-items: center;
 }
 .direction{
-  padding: 10px;
+  padding: 22px;
   border-radius: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
 }
 .fill{
   display: flex;
@@ -155,7 +156,7 @@ export default {
   justify-content: center;
   align-items: center;
 }
-.dialog{
+.logo1{
   padding: 5px;
 }
 .dialogButton{
