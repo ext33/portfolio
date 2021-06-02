a34516adcfec28211fa4a59e4ef1864b245d3a18
ext33 <redwoodx3@gmail.com>
2021-06-02 23:38:29 +0300

    add stack refs

--- front/src/pages/About/index.js
+++ front/src/pages/About/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import './AboutPage.sass'
+import reactLogo from '../../assets/react-icon.svg'
+import vueLogo from '../../assets/vue-logo.svg'
+import nodeLogo from '../../assets/nodejs-icon.svg'
+import vsLogo from '../../assets/vs-logo.png'
 
 function AboutPage(props) {
     return (
@@ -10,6 +14,45 @@ function AboutPage(props) {
             </div>
             <div className="about-page__desc">
                 {props.text.about.desc}
+            </div>
+            <div className="flex about-page__stack">
+
+                <div className="flex about-page__stack-item">
+                    <div className="about-page__stack-item-icon">
+                        <img src={reactLogo} alt="react" />
+                    </div>
+                    <div className="about-page__stack-item-desc">
+                        {props.text.about.react}
+                    </div>
+                </div>
+
+                <div className="flex about-page__stack-item">
+                    <div className="about-page__stack-item-desc">
+                        {props.text.about.vue}
+                    </div>
+                    <div className="about-page__stack-item-icon">
+                        <img src={vueLogo} alt="vue" />
+                    </div>
+                </div>
+
+                <div className="flex about-page__stack-item">
+                    <div className="about-page__stack-item-icon">
+                        <img src={nodeLogo} alt="node" />
+                    </div>
+                    <div className="about-page__stack-item-desc">
+                        {props.text.about.node}
+                    </div>
+                </div>
+
+                <div className="flex about-page__stack-item">
+                    <div className="about-page__stack-item-desc">
+                        {props.text.about.vsc}
+                    </div>
+                    <div className="about-page__stack-item-icon">
+                        <img src={vsLogo} alt="vs-code" />
+                    </div>
+                </div>
+
             </div>
         </div>
     )
--- front/src/store/languageStore/content.js
+++ front/src/store/languageStore/content.js
@@ -9,11 +9,15 @@ export const en = {
     },
     'about': {
         'title': 'About',
-        'desc': '',
+        'desc': 'Currently developing in the direction of frontend development, I have experience working with React, Redux, Vue, Node JS, Django, participated in startups and hackathons.',
+        'react': 'React is a declarative, efficient, and flexible JavaScript library for creating user interfaces. It allowed me to develop SPA applications efficiently and flexibly.',
+        'vue': 'Vue is a progressive framework for creating user interfaces. Simplifies integration with other libraries and existing projects. It allows you to quickly develop applications with external UI frameworks.',
+        'node': 'As an asynchronous JavaScript runtime, Node.js is designed to create scalable web applications. This technology allowed me to create server applications in JavaScript.',
+        'vsc': 'Visual Studio Code is a source code editor developed by Microsoft. Allows me to write JS code efficiently and quickly.'
     },
     'work': {
         'title': 'Work',
-        'desc': '',
+        'desc': 'I have 3 full-fledged projects using frameworks. In my projects, I used Vue, React, Redux, Node + Express, Gulp.',
     },
 }
 
@@ -28,10 +32,14 @@ export const ru = {
     },
     'about': {
         'title': 'Обо мне',
-        'desc': '',
+        'desc': 'В данный момент развиваюсь в направлении frontend разработки, имею опыт работы с React, Redux, Vue, NodeJS, Django, учавствовал в стартапах и хакатонах.',
+        'react': 'React — это декларативная, эффективная и гибкая JavaScript библиотека для создания пользовательских интерфейсов. Она позволила мне эффективно и гибко разрабатывать SPA-приложения.',
+        'vue': 'Vue — это прогрессивный фреймворк для создания пользовательских интерфейсов. Упрощает интеграцию с другими библиотеками и существующими проектами. Позволяет довольно быстро разрабатывать приложения с внешними UI-фреймворками.',
+        'node': 'Как асинхронная среда выполнения JavaScript, Node.js предназначен для создания масштабируемых веб-приложений. Данная технология позволила мне создавать серверные приложения на JavaScript.',
+        'vsc': 'Visual Studio Code — редактор исходного кода, разработанный компанией Microsoft. Позволяет мне эффективно и быстро писать JS-код.'
     },
     'work': {
         'title': 'Работы',
-        'desc': '',
+        'desc': 'Имею 3 полноценных проекта с использованием фреймворков. В своих проектах я использовал Vue, React, Redux, Node + Express, Gulp.',
     },
 }
